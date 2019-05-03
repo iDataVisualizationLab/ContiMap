7cad144d78f808ded478409d6ec8d2d5c3c46e53
phamvanvung <phamvanvung@gmail.com>
2019-05-02 22:12:17 -0500

    Optimized draw function for better performance.

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,9 +2,9 @@
 const FIELD_MACHINE_ID = 'machine_id';
 const FIELD_TIME_STAMP = 'time_stamp';
 /**HPCC**/
-// const VARIABLES = ["CPU1 Temp", "Fan1 speed", "Power consumption"];
+const VARIABLES = ["CPU1 Temp", "Fan1 speed", "Power consumption"];
 /**Alibaba**/
-const VARIABLES = ["cpu_util_percent"];
+// const VARIABLES = ["cpu_util_percent"];
 
 /**Configuration**/
 const oneWay = true;
